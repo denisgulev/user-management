3a22630e3c1a22b0626b721bd28a2eb45193ba0b
denisgulev <denis_13@hotmail.it>
2024-11-09 00:35:16 +0100

    refactoring JWT management and logs

--- src/main/kotlin/example/com/Application.kt
+++ src/main/kotlin/example/com/Application.kt
@@ -2,14 +2,12 @@ package example.com
 
 import example.com.plugins.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.callloging.*
-import io.ktor.server.request.*
-import org.slf4j.event.Level
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+@Suppress("unused") // Referenced in application.conf
 fun Application.module() {
     configureSerialization()
     configureDI(environment)
